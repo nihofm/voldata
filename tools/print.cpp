6b675f069322b535e6d1c4699de20f97c6a29e83
Nikolai Hofmann <nikolai.hofmann@fau.de>
2021-09-20 11:05:29 +0200

    Makefile update, print.cpp load/conversion times

--- tools/print.cpp
+++ tools/print.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <memory>
+#include <chrono>
 #include <glm/gtx/string_cast.hpp>
 
 #include "voldata.h"
@@ -9,22 +10,30 @@ int main(int argc, char** argv) {
 
     std::cout << "------------------" << std::endl;
     std::cout << "Volume:" << std::endl;
+    auto start_load = std::chrono::system_clock::now();
     std::shared_ptr<voldata::Volume> volume = std::make_shared<voldata::Volume>(path);
+    auto end_load = std::chrono::system_clock::now();
     std::cout << *volume << std::endl;
 
     std::cout << "------------------" << std::endl;
     std::cout << "Dense grid:" << std::endl;
+    auto start_dense = std::chrono::system_clock::now();
     std::shared_ptr<voldata::DenseGrid> dense_grid = std::make_shared<voldata::DenseGrid>(volume->current_grid());
+    auto end_dense = std::chrono::system_clock::now();
     std::cout << *dense_grid << std::endl;
 
     std::cout << "------------------" << std::endl;
     std::cout << "VDB grid:" << std::endl;
+    auto start_vdb = std::chrono::system_clock::now();
     std::shared_ptr<voldata::OpenVDBGrid> vdb_grid = std::make_shared<voldata::OpenVDBGrid>(volume->current_grid());
+    auto end_vdb = std::chrono::system_clock::now();
     std::cout << *vdb_grid << std::endl;
 
     std::cout << "------------------" << std::endl;
     std::cout << "Brick grid:" << std::endl;
+    auto start_brick = std::chrono::system_clock::now();
     std::shared_ptr<voldata::BrickGrid> brick_grid = std::make_shared<voldata::BrickGrid>(volume->current_grid());
+    auto end_brick = std::chrono::system_clock::now();
     std::cout << *brick_grid << std::endl;
 
     std::cout << "------------------" << std::endl;
@@ -33,5 +42,11 @@ int main(int argc, char** argv) {
     std::cout << "lookup dense " << glm::to_string(ipos) << ": " << dense_grid->lookup(ipos) << std::endl;
     std::cout << "lookup vdb " << glm::to_string(ipos) << ": " << vdb_grid->lookup(ipos) << std::endl;
     std::cout << "lookup brick " << glm::to_string(ipos) << ": " << brick_grid->lookup(ipos) << std::endl;
+
+    std::cout << "------------------" << std::endl;
+    std::cout << "Loading took " << (end_load - start_load).count() / 1000000 << "ms." << std::endl;
+    std::cout << "Dense grid conversion took " << (end_dense - start_dense).count() / 1000000 << "ms." << std::endl;
+    std::cout << "Brick grid conversion took " << (end_brick - start_brick).count() / 1000000 << "ms." << std::endl;
+    std::cout << "VDB grid conversion took " << (end_vdb - start_vdb).count() / 1000000 << "ms." << std::endl;
     return 0;
 }
